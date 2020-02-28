486eb1bb5cbefe683e7288150f9acb7c43115750
Sean E. Russell <ser@ser1.net>
2020-02-28 10:17:39 -0600

    Initial commit

---
+++ dummy.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/xxxserxxx/gotop/devices"
+)
+
+func Init() {
+	devices.RegisterTemp(updateTemp)
+	devices.RegisterMem(updateMem)
+	devices.RegisterCPU(updateUsage)
+}
+
+func updateTemp(temps map[string]int) map[string]error {
+	temps["Crazy1"] = rand.Intn(50) + 50
+	temps["Crazy2"] = rand.Intn(50) + 50
+	return nil
+}
+
+func updateMem(mems map[string]devices.MemoryInfo) map[string]error {
+	m := uint64(rand.Intn(80e8))
+	max := uint64(80e8)
+	mems["Dum1"] = devices.MemoryInfo{
+		Total:       max,
+		Used:        m,
+		UsedPercent: (float64(m) / float64(max)) * 100,
+	}
+	return nil
+}
+
+func updateUsage(cpus map[string]int, _ time.Duration, _ bool) map[string]error {
+	cpus["Wopper01"] = rand.Intn(100)
+	cpus["Wopper02"] = rand.Intn(100)
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/xxxserxxx/gotop-dummy
+
+require github.com/xxxserxxx/gotop v0.0.0-20200228130651-7a89225d729d
+
+replace github.com/xxxserxxx/gotop => ../gotop
+
+go 1.13
